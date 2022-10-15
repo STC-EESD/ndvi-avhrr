b80d0e6f2df532ddb5284a84a133ef82f5240db4
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-10-15 11:13:15 -0400

    updated: wget-ndvi-avhrr.sh

--- 901-download-ndvi-avhrr/wget-ndvi-avhrr.sh
+++ 901-download-ndvi-avhrr/wget-ndvi-avhrr.sh
@@ -1,13 +1,40 @@
 #!/bin/bash
 
-wget --no-check-certificate https://ftp.maps.canada.ca/pub/statcan_statcan/avhrr/Bands_Weeks_AVHRR_Semaines_Bandes.xlsx
+server_url=https://ftp.maps.canada.ca/pub/statcan_statcan/avhrr
+
+wget --no-check-certificate ${server_url}/Bands_Weeks_AVHRR_Semaines_Bandes.xlsx
+
+echo
+echo start: downloads `date`
+echo
 
 # for YEAR in {1987..2022..1}
 for YEAR in {1987..1991..1}
 do
-    tempfile=https://ftp.maps.canada.ca/pub/statcan_statcan/avhrr/AVHRR1KM_${YEAR}.zip
+    tempfile=${server_url}/AVHRR1KM_${YEAR}.zip
     echo
     echo downloading: ${tempfile}
     wget --no-check-certificate ${tempfile}
 done
+
+echo
+echo completed: downloads `date`
+echo
+
+echo
+echo start: unzipping `date`
+echo
+
+# for YEAR in {1987..2022..1}
+for YEAR in {1987..1991..1}
+do
+    tempfile=AVHRR1KM_${YEAR}.zip
+    echo
+    echo unzipping: ${tempfile}
+    unzip ${tempfile}
+done
+echo
+
+echo
+echo completed: unzipping `date`
 echo
