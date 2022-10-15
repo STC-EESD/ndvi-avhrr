e71b643aeefa4ccb329cf02bbf1f288fc61c627a
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-10-15 13:52:33 -0400

    added: 000-data/001-ndvi-avhrr/main.sh

---
+++ 000-data/001-ndvi-avhrr/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##################################################
+currentDIR=`pwd`
+   codeDIR=${currentDIR}/code
+ outputDIR=${currentDIR//github/gittmp}/output
+
+parentDIR=`dirname ${currentDIR}`
+  dataDIR=${parentDIR}/000-data
+
+if [ ! -d ${outputDIR} ]; then
+        mkdir -p ${outputDIR}
+fi
+
+cp -r ${codeDIR} ${outputDIR}
+cp    $0         ${outputDIR}/code
+
+##################################################
+cd ${outputDIR}
+sleep 2
+myShellScript=./code/wget-ndvi-avhrr.sh
+stdoutFile=stdout.sh.`basename ${myRscript} .sh`
+stderrFile=stderr.sh.`basename ${myRscript} .sh`
+myShellScript > ${stdoutFile} 2> ${stderrFile}
