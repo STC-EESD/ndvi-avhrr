f900fc064f385cc8a30150c182a21b64ad69671c
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-10-30 22:09:52 -0400

    updated: getMetaData.R

--- 001-explore/code/getMetaData.R
+++ 001-explore/code/getMetaData.R
@@ -35,39 +35,41 @@ getMetaData_spatial <- function(
     n.geotiffs <- length(geotiffs);
 
     DF.geotiffs <- data.frame(
-        year    = as.numeric(stringr::str_extract(string = geotiffs, pattern = "[0-9]{4}")),
-        geotiff = geotiffs,
-        n.x     = numeric(  length = n.geotiffs),
-        n.y     = numeric(  length = n.geotiffs),
-        x.min   = numeric(  length = n.geotiffs),
-        x.max   = numeric(  length = n.geotiffs),
-        y.min   = numeric(  length = n.geotiffs),
-        y.max   = numeric(  length = n.geotiffs),
-        crs     = character(length = n.geotiffs)
+        year     = as.numeric(stringr::str_extract(string = geotiffs, pattern = "[0-9]{4}")),
+        geotiff  = geotiffs,
+        n.layers = integer(  length = n.geotiffs),
+        n.x      = integer(  length = n.geotiffs),
+        n.y      = integer(  length = n.geotiffs),
+        x.min    = integer(  length = n.geotiffs),
+        x.max    = integer(  length = n.geotiffs),
+        y.min    = integer(  length = n.geotiffs),
+        y.max    = integer(  length = n.geotiffs),
+        crs      = character(length = n.geotiffs)
         );
     DF.geotiffs <- DF.geotiffs[order(DF.geotiffs[,'year']),];
     rownames(DF.geotiffs) <- NULL;
 
     for ( row.index in seq(1,nrow(DF.geotiffs)) ) {
-        temp.path   <- file.path(data.directory,DF.geotiffs[row.index,'geotiff']);
-        temp.raster <- raster::raster(temp.path);
-        temp.coords <- raster::coordinates(temp.raster);
-        unique.x    <- unique(temp.coords[,1]);
-        unique.y    <- unique(temp.coords[,2]);
-        DF.geotiffs[row.index,'n.x']   <- length(unique.x);
-        DF.geotiffs[row.index,'n.y']   <- length(unique.y);
-        DF.geotiffs[row.index,'x.min'] <- min(unique.x);
-        DF.geotiffs[row.index,'x.max'] <- max(unique.x);
-        DF.geotiffs[row.index,'y.min'] <- min(unique.y);
-        DF.geotiffs[row.index,'y.max'] <- max(unique.y);
-        DF.geotiffs[row.index,'crs']   <- as.character(raster::crs(temp.raster));
+        temp.path   <- base::file.path(data.directory,DF.geotiffs[row.index,'geotiff']);
+        temp.brick <- raster::brick(temp.path);
+        temp.coords <- raster::coordinates(temp.brick);
+        unique.x    <- base::unique(temp.coords[,1]);
+        unique.y    <- base::unique(temp.coords[,2]);
+        DF.geotiffs[row.index,'n.layers'] <- raster::nlayers(temp.brick);
+        DF.geotiffs[row.index,'n.x'     ] <- base::length(unique.x);
+        DF.geotiffs[row.index,'n.y'     ] <- base::length(unique.y);
+        DF.geotiffs[row.index,'x.min'   ] <-    base::min(unique.x);
+        DF.geotiffs[row.index,'x.max'   ] <-    base::max(unique.x);
+        DF.geotiffs[row.index,'y.min'   ] <-    base::min(unique.y);
+        DF.geotiffs[row.index,'y.max'   ] <-    base::max(unique.y);
+        DF.geotiffs[row.index,'crs'     ] <- base::as.character(raster::crs(temp.brick));
         }
 
     cat("\nDF.geotiffs\n");
     print( DF.geotiffs   );
 
-    cat("\nunique(DF.geotiffs[,setdiff(colnames(DF.geotiffs),c('year','geotiff'))])\n");
-    print( unique(DF.geotiffs[,setdiff(colnames(DF.geotiffs),c('year','geotiff'))])   );
+    cat("\nunique(DF.geotiffs[,setdiff(colnames(DF.geotiffs),c('year','geotiff','n.layers'))])\n");
+    print( unique(DF.geotiffs[,setdiff(colnames(DF.geotiffs),c('year','geotiff','n.layers'))])   );
 
     return( NULL );
 
